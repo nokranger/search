6df17f8536c7f1de9dfecc65cbf5bfd530f41d6c
nokranger <anusorn.thavornporn@gmail.com>
2018-11-28 09:07:08 +0700

    first time for search

---
+++ search.php
@@ -0,0 +1,436 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="robots" content="noindex, nofollow">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Contruction Business Directory</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <style type="text/css">
+    </style>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+    <script src='//production-assets.codepen.io/assets/editor/live/console_runner-079c09a0e3b9ff743e39ee2d5637b9216b3545af0de366d4b9aad9dc87e26bfd.js'></script>
+    <script src='//production-assets.codepen.io/assets/editor/live/events_runner-73716630c22bbc8cff4bd0f07b135f00a0bdc5d14629260c3ec49e5606f98fdd.js'></script>
+    <script src='//production-assets.codepen.io/assets/editor/live/css_live_reload_init-2c0dc5167d60a5af3ee189d570b1835129687ea2a61bee3513dee3a50c115a77.js'></script>
+    <link rel="shortcut icon" type="image/x-icon" href="//production-assets.codepen.io/assets/favicon/favicon-8ea04875e70c4b0bb41da869e81236e54394d63638a1ef12fa558a4a835f1164.ico" />
+    <link rel="mask-icon" type="" href="//production-assets.codepen.io/assets/favicon/logo-pin-f2d2b6d2c61838f7e76325261b7195c27224080bc099486ddd6dccb469b8e8e6.svg"
+        color="#111" />
+    <link rel="canonical" href="https://codepen.io/ericwinton/pen/jqKyyq?depth=everything&order=popularity&page=7&q=product&show_forks=false" />
+    <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.5.0/css/all.css' integrity='sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU'
+        crossorigin='anonymous'>
+    <link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css'>
+<style class="cp-pen-styles">
+body {
+	padding-top: 20px;
+}
+.product {
+	margin-bottom: 20px;
+}
+.product-inner {
+	box-shadow: 0 0 10px rgba(0,0,0,.2);
+	padding: 10px;
+}
+/* .product img {
+    margin-bottom: 10px;
+
+} */
+img {
+    height: 170px;
+    width: 200px;
+    margin:auto;
+    margin-bottom:10px;
+    margin-top:10px;
+    display:block;
+
+}
+span {
+
+    margin-left:10px;
+}
+</style>
+</head>
+<body>
+<?php
+header("content-type:charset=utf-8");
+// Initialize variable for database credentials
+$dbhost = '127.0.0.1';
+$dbuser = 'root';
+$dbpass = '';
+$dbname = 'oskb';
+//Create database connection
+$dblink = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+$sql = "SELECT `AN_Name`,`AN_Address`,`Entity_Name` FROM `announce` as d1 INNER JOIN `entity_has_announce` as d2 ON d1.AN_ID = d2.announce_AN_ID INNER JOIN `entity` as d3 ON d2.entity_ID_Entity = d3.ID_Entity";
+$sqll = ("SELECT * FROM `business_s`");
+$sqlll =("SELECT `AN_Name`,`AN_Address`,`Entity_Name` FROM `announce` as d1 INNER JOIN `entity_has_announce` as d2 ON d1.AN_ID = d2.announce_AN_ID INNER JOIN `entity` as d3 ON d2.entity_ID_Entity = d3.ID_Entity");
+$sqllll = ("SELECT `AN_Name`,`Entity_Name`,`AN_Address`,`P_Name` FROM `announce` as d1 
+INNER JOIN `entity_has_announce` as d2 
+ON d1.AN_ID = d2.announce_AN_ID 
+INNER JOIN `entity` as d3 
+ON d2.entity_ID_Entity = d3.ID_Entity 
+INNER JOIN `pictures` as d4");
+$x =0;
+//Check connection was successful
+  if ($dblink->connect_errno) {
+     printf("Failed to connect to database");
+     exit();
+  }
+//Fetch 3 rows from actor table
+  $dblink->set_charset("utf8");
+  $result = $dblink->query($sqllll);
+  
+//   foreach($result as $sql2){
+//     $make[$x] = $sql2['AN_Name'];
+//     $sqll = "SELECT `Entity_Name` FROM `entity` as d1 INNER JOIN `entity_has_announce` as d2 ON d1.`ID_Entity`= d2.entity_ID_Entity WHERE `announce_AN_ID` = ".$sql2['AN_ID'];
+//     $result2 = $dblink->query($sqll);
+//     $y=0;
+//     foreach($result2 as $sq){
+//     $model[$x][$y] = $sq['Entity_Name'];
+//     $y++;
+//     }
+//     $type[$x] = $sql2['AN_Address'];
+//     $x++;
+//     echo json_encode($sql2 + $sq);
+// }
+//Initialize array variable
+  $dbdata = array();
+//Fetch into associative array
+  while ( $row = $result -> fetch_assoc())  {
+    $dbdata[]=$row;
+    $test = $row;
+  }
+//   while ( $row = $result2->fetch_assoc()){
+//     $dbdataa[] = $row;  
+//   }
+//   var_dump($test);
+  //$dataencode = json_encode($dbdata);
+//Print array in JSON format
+// echo json_encode($dbdata);
+print_r($dbdata);
+?>
+    <div class="container">
+        <div class="row" id="search">
+            <form id="search-form" action="" method="POST" enctype="multipart/form-data">
+                <div class="form-group col-xs-9">
+                    <input class="form-control text-center" type="text" placeholder="Search" />
+                </div>
+                <div class="form-group col-xs-3 col-xs-6">
+                    <button type="submit" class="btn btn-block btn-success">Search</button>
+                </div>
+            </form>
+        </div>
+        <div class="row" id="filter">
+            <form>
+                <div class="form-group col-sm-3 col-xs-6">
+                    <select data-filter="make" class="filter-make filter form-control">
+                        <option value="">Select Business</option>
+                        <option value="">Show All</option>
+                    </select>
+                </div>
+                <!-- <div class="form-group col-sm-3 col-xs-6">
+                    <select data-filter="model" class="filter-model filter form-control">
+                        <option value="">Select Sup group</option>
+                        <option value="">Show All</option>
+                    </select>
+                </div> -->
+                <div class="form-group col-sm-3 col-xs-6">
+                    <select data-filter="type" class="filter-type filter form-control">
+                        <option value="">Select Location</option>
+                        <option value="">Show All</option>
+                    </select>
+                </div>
+            </form>
+        </div>
+        <div class="row" id="products">
+
+        </div>
+    </div>
+    <script src='//production-assets.codepen.io/assets/common/stopExecutionOnTimeout-b2a7b3fe212eaa732349046d8416e00a9dec26eb7fd347590fbced3ab38af52e.js'></script>
+    <script src='//cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.min.js'></script>
+    <script src='//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js'></script>
+    <script>
+        var data2 = <?php echo json_encode($dbdata); ?>;
+        // var newData = JSON.stringify(data);
+        // console.log(typeof(newData));
+        // console.log(newData);
+//         data2 = 
+//             [{"AN_ID":"4","AN_Name":"PRIME Wellness Group Co., Ltd.","AN_Address":null,"Entity_Name":"งานปรับปรุง ซ่อมแซม ต่อเติม"},
+//                 {"AN_ID":"4","AN_Name":"PRIME Wellness Group Co., Ltd.","AN_Address":null,"Entity_Name":"ภูมิสถาปัตย์"},
+//                 {"AN_ID":"4","AN_Name":"PRIME Wellness Group Co., Ltd.","AN_Address":null,"Entity_Name":"อินทีเรีย"},
+//                 {"AN_ID":"4","AN_Name":"PRIME Wellness Group Co., Ltd.","AN_Address":null,"Entity_Name":"โรงงาน/โกดัง"},
+//                 {"AN_ID":"4","AN_Name":"PRIME Wellness Group Co., Ltd.","AN_Address":null,"Entity_Name":"โรงแรม/รีสอร์ท"},
+//                 {"AN_ID":"4","AN_Name":"PRIME Wellness Group Co., Ltd.","AN_Address":null,"Entity_Name":"อาคารประเภทอื่นๆ"},
+//                 {"AN_ID":"4","AN_Name":"PRIME Wellness Group Co., Ltd.","AN_Address":null,"Entity_Name":"สถาปนิก"},
+//                 {"AN_ID":"4","AN_Name":"PRIME Wellness Group Co., Ltd.","AN_Address":null,"Entity_Name":"งานตกแต่งสวน"},
+//                 {"AN_ID":"4","AN_Name":"PRIME Wellness Group Co., Ltd.","AN_Address":null,"Entity_Name":"ที่ปรึกษาควบคุมงาน"},
+//                 {"AN_ID":"4","AN_Name":"PRIME Wellness Group Co., Ltd.","AN_Address":null,"Entity_Name":"งานตกแต่งภายใน"},
+//                 {"AN_ID":"4","AN_Name":"PRIME Wellness Group Co., Ltd.","AN_Address":null,"Entity_Name":"อาคารสำนักงาน"},
+//                 {"AN_ID":"4","AN_Name":"PRIME Wellness Group Co., Ltd.","AN_Address":null,"Entity_Name":"บ้าน/อาคารที่พักอาศัย"},
+//                 {"AN_ID":"5","AN_Name":"Mycollection","AN_Address":null,"Entity_Name":"งานตกแต่งภายใน"},
+// {"AN_ID":"5","AN_Name":"Mycollection","AN_Address":null,"Entity_Name":"บ้าน/อาคารที่พักอาศัย"},
+// {"AN_ID":"5","AN_Name":"Mycollection","AN_Address":null,"Entity_Name":"งานปรับปรุง ซ่อมแซม ต่อเติม"},
+// {"AN_ID":"5","AN_Name":"Mycollection","AN_Address":null,"Entity_Name":"โรงแรม/รีสอร์ท"}];
+                //this for change value string entity to array
+                data33 ="img/";
+                for(i=0;i<data2.length;i++){
+                    data22 = [data2[i].Entity_Name];
+                    // data33 = JSON.stringify("img/" + data2[i].P_Name);
+                    data33 = data33+data2[i].P_Name;
+                    data2[i].Entity_Name = data22
+                    data2[i].P_Name = data33;
+                }
+                console.log(data2[0].P_Name);
+                console.log(data33);
+                
+        // data22 = [];
+        // for(i=0;i<data2.length;i++){        
+        // data22.AN_ID = data2[i].AN_ID;
+        // data22 = data2[i].AN_Name;
+        // data22 = data2[i].AN_Address;
+        // data22 = data2[i].Entity_Name;
+        // // data2 = data22;
+        // // data22.push(data2[i].Entity_Name);
+        // // data2[i].Entity_Name = data22[i];
+        
+        // data2.push(data22);
+        
+        
+        // }
+        // let json = jQuery.parseJSON(JSON.stringify(data2));
+        // let json2;
+        //     json2 = JSON.parse(data2);
+        //     data2 = JSON.stringify(json2);
+        //     data2 = json2;
+
+        // console.log(json2);
+        // data2[0].Entity_Name = data22;
+        // console.log(data22);
+        console.log(data2);
+        // data222 = jQuery.parseJSON(JSON.stringify(data2[0].Entity_Name));
+        // data2[0].Entity_Name =  data222;
+        // console.log(data222);
+        // console.log(data2);
+        // console.log(data222);
+        // console.log(data22);
+        // console.log(data22);
+        
+        // dataa = "";
+        // for(i=0;i<data2.length;i++){
+        //     dataa = data2[i].Entity_Name;
+        //     console.log(dataa[i]);
+        //     data2[i].Entity_Name = dataa;
+        //     data2[i] = jQuery.parseJSON(JSON.stringify(data2));
+        //     console.log(data2);
+        // }
+        // dataa = data2[0].Entity_Name;
+        // console.log(data2);
+        // console.log(dataa);
+    
+        // for(i = 0;i<data2.length;i++){
+        //     dataa = jQuery.parseJSON(JSON.stringify(data2));
+        //     var datatest = JSON.stringify(data2[0]);
+        //     // data2[i].Entity_Name = dataa;
+        //     // console.log(data3);
+        //     // console.log(data2);
+        //     // var yourval = jQuery.parseJSON(JSON.stringify(data2[i]));
+        //     // console.log(yourval);
+        //     // console.log(data2);
+            
+        // }
+
+        // console.log(dataa);
+        // dataa = JSON.stringify('[' + data2[0].Entity_Name +']');
+        // console.log(dataa);
+        // var yourval = jQuery.parseJSON(JSON.stringify(data2));
+        // console.log(yourval);
+        // console.log(yourval);
+
+        // var yourval = jQuery.parseJSON(JSON.stringify(data));
+        // console.log(newData);
+        // console.log(data4);
+        // console.log(data2);
+        // data = JSON.parse(data);
+        // console.log(typeof(data));
+        // console.log(data);
+        // var data = [
+        // 	{
+        // 		"make": "PRIME Wellness Group Co., Ltd.",
+        // 		"model": ["งานก่อสร้างอาคาร","jtrjtrjrtkrtk"],
+        // 		"type": "นราธิวาส",
+        // 		"image": "img/fff.png"
+        // 	},
+        // 	{
+        // 		"make": "Mycollection",
+        // 		"model": ["(*&^%$#W","85975650o6po87pt"],
+        // 		"type": "ปทุมธานี",
+        // 		"image": "http://placehold.it/800x600/418cf4/fff"
+        // 	},
+        // 	{
+        // 		"make": "เอชเคเนชั่นแนลโปรดัคทส์จำกัด",
+        // 		"model": ["54uj5ekj[e[rkjeork","yweryhsrfjdfj"],
+        // 		"type": "นนทบุรี",
+        // 		"image": "http://placehold.it/800x600/418cf4/fff"
+        // 	},
+        // 	{
+        // 		"make": "Proffer Complement Co.,Ltd",
+        // 		"model": ["hrhwrhsehaehad","qy43wyrewuhystrut"],
+        // 		"type": "นครปฐม",
+        // 		"image": "http://placehold.it/800x600/418cf4/fff"
+        // 	},
+        // 	{
+        // 		"make": "First Floor Consultants Co.ltd.",
+        // 		"model": ["y4wu5ei5irt","Y4A3EURSEDJDFJ"],
+        // 		"type": "นครศรีธรรมราช",
+        // 		"image": "http://placehold.it/800x600/418cf4/fff"
+        // 	},
+        // 	{
+        // 		"make": "97 Home Builder",
+        // 		"model": ["gshshjsjs","jtrdjfgcjnfcvn"],
+        // 		"type": "ภูเก็ต",
+        // 		"image": "http://placehold.it/800x600/418cf4/fff"
+        // 	},
+        // 	{
+        // 		"make": "Inter management co.ltd.",
+        // 		"model": ["เำำไเ้ำไ้ำไ้ไำ้","hershxdfvnxcvn"],
+        // 		"type": "พังงา",
+        // 		"image": "http://placehold.it/800x600/418cf4/fff"
+        //     },
+        //     {
+        // 		"make": "Admin %$#@@@!",
+        // 		"model": ["แอร์","45ktfgkfgmghm","gewgwegewaaa"],
+        // 		"type": "กรุงเทพมหานคร",
+        // 		"image": "http://placehold.it/800x600/418cf4/fff"
+        // 	},
+        //     {
+        // 		"make": "Admin %$#@@@!",
+        // 		"model": ["พัดลม","กาแฟ","โอวัลติน"],
+        // 		"type": "กรุงเทพมหานคร",
+        // 		"image": "http://placehold.it/800x600/418cf4/fff"
+        // 	},
+        // 	{
+        // 		"make": "Inter management co.ltd.",
+        // 		"model": ["่ภถไ่ำพ่พะ่พะก่","้ำไ้ำ่พะ่พะ่"],
+        // 		"type": "พังงา",
+        // 		"image": "http://placehold.it/800x600/418cf4/fff"
+        //     }
+        // ];
+        var arr = data2.reduce((acc,cur) => {
+        var index = acc.findIndex(el => el.AN_Name === cur.AN_Name);
+            if (index > -1){
+                    acc[index].Entity_Name = [...acc[index].Entity_Name, ...cur.Entity_Name]
+                    return acc
+                      }else{
+                            return [...acc, cur]
+            }
+}, [])
+        // for(i=0;i<arr.length;i++){
+        //     for(j=0;j<){
+
+        //     }
+        // }
+        // var data = arr;
+        // var newData = JSON.stringify(arr)
+        // var newData2 = JSON.stringify(data)
+        console.log(typeof(arr));
+        console.log(arr);
+
+
+        var products = "";
+        var makes = "";
+        var models = "";
+        var types = "";
+        //consol.log(data);
+        // alert(data['make']);
+        for (var i = 0; i < data.length; i++) {
+            for(var j=0;j<data[i].model.length;j++){
+
+            if (window.CP.shouldStopExecution(1)) { break; }
+            var make = data[i].make,
+                type = data[i].type,
+                image = data[i].image;
+                model = data[i].model[j];
+
+              
+            //create dropdown of makes
+            if (makes.indexOf("<option value='" + make + "'>" + make + "</option>") == -1) {
+                makes += "<option value='" + make + "'>" + make + "</option>";
+            }
+
+            //create dropdown of models
+
+            if (models.indexOf("<option value='" + data[i].model[j] +  "'>" + data[i].model[j] + "</option>") == -1) {
+                models += "<option value='" + data[i].model[j] + "'>" + data[i].model[j] + "</option>";
+            }
+            
+
+            //create dropdown of types
+            if (types.indexOf("<option value='" + type + "'>" + type + "</option>") == -1) {
+                types += "<option value='" + type + "'>" + type + "</option>";
+            }
+        }
+                    //create product cards
+        products += "<div class='col-sm-4 product' data-make='" + make + "' data-model='" + model + "' data-type='" + type + "'><div class='product-inner text-left'><img src='" + image + "'><br /><div><span class='fas fa-landmark' style='font-size:24px'></span> : " + make + "</div><br /><div><span class='fas fa-briefcase' style='font-size:24px'></span> : " + data[i].model[0]  + "</div><br /><div><span class='fas fas fa-map-marker-alt' style='font-size:24px'></span> : " + type + "</div><br /></div></div>";
+        }
+        window.CP.exitedLoop(1);
+
+
+        $("#products").html(products);
+        $(".filter-make").append(makes);
+        $(".filter-model").append(models);
+        $(".filter-type").append(types);
+
+        var filtersObject = {};
+
+        //on filter change
+        $(".filter").on("change", function () {
+            var filterName = $(this).data("filter"),
+                filterVal = $(this).val();
+
+            if (filterVal == "") {
+                delete filtersObject[filterName];
+            } else {
+                filtersObject[filterName] = filterVal;
+            }
+
+            var filters = "";
+
+            for (var key in filtersObject) {
+                if (window.CP.shouldStopExecution(2)) { break; }
+                if (filtersObject.hasOwnProperty(key)) {
+                    filters += "[data-" + key + "='" + filtersObject[key] + "']";
+                }
+            }
+            window.CP.exitedLoop(2);
+
+
+            if (filters == "") {
+                $(".product").show();
+            } else {
+                $(".product").hide();
+                $(".product").hide().filter(filters).show();
+            }
+        });
+
+        //on search form submit
+        $("#search-form").submit(function (e) {
+            e.preventDefault();
+            var query = $("#search-form input").val().toLowerCase();
+
+            $(".product").hide();
+            $(".product").each(function () {
+                var make = $(this).data("make").toLowerCase(),
+                    model = $(this).data("model").toLowerCase(),
+                    type = $(this).data("type").toLowerCase();
+
+                if (make.indexOf(query) > -1 || model.indexOf(query) > -1 || type.indexOf(query) > -1) {
+                    $(this).show();
+                }
+            });
+        });
+//# sourceURL=pen.js
+    </script>
+</body>
+
+</html>
